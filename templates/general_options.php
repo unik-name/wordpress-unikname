17034a771963e60fe64b42a638e4feb51b784116
Damien Lecan <dev@dlecan.com>
2020-12-09 21:43:10 +0100

    feat: update baseline and titles

--- templates/general_options.php
+++ templates/general_options.php
@@ -3,7 +3,7 @@
         <div class="left">
             <div class="content-left">
                 <img src="<?=UNIKNAME_DIR_URL.'assets/images/logo-unikname.png'?>" alt="<?=__('Logo Unik Name','unikname-connect')?>">
-                <h3><?=__('is a next-generation authentication solution protecting your business and your user accounts from hacks and data theft','unikname-connect')?></h3>
+                <h3><?=__('The plugin that secures your admin accounts and that rewards your website users for protecting their privacy','unikname-connect')?></h3>
             </div>
         </div>
         <div class="right">
