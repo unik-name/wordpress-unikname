a2ecd1e362c4cdb97aa24b90239b067cfe78c946
Damien Lecan <dev@dlecan.com>
2021-03-05 16:01:39 +0100

    chore: add new envvar

--- unikname_connect.php
+++ unikname_connect.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Unikname Connect - Secure and Rewarded Connections
+Plugin Name: Unikname Connect
 Plugin URI: https://wordpress.org/plugins/unikname-connect/
-Description: Secure your admin accounts and reward your website users for protecting their privacy. Integrate the famous Unikname Connect login solution into your Wordpress or WooCommerce websites.
+Description: Secure your admin accounts and reward your website users for protecting their privacy
 Version: 8.4.0
 Author: Unikname
 Author URI: https://www.unikname.com
@@ -11,7 +11,8 @@ Domain Path: /languages
 License: GPL2+
 */
 defined('ABSPATH') or die("Cheating........Uh!!");
-define('THE_CHAMP_SS_VERSION', '8.4.0');
+define('UNIKNAME_VERSION', '8.4.0');
+define('THE_CHAMP_SS_VERSION', UNIKNAME_VERSION);
 
 if (!defined('UNIKNAME_CONNECT_SERVER')) {
 	define('UNIKNAME_CONNECT_SERVER', getenv('UNIKNAME_CONNECT_SERVER') ?: 'https://connect.unikname.com');
